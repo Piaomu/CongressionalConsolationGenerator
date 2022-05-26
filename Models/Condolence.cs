bff166a88ad3f433b278a2c98f1686a1a0520c5a
piaomu <driftwood.wahl@gmail.com>
2022-05-26 06:20:19 -0500

    Add project files.

---
+++ Models/Condolence.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace CongressionalConsolationGenerator.Models
+{
+    public class Condolence
+    {
+        public Subject SentenceSubject { get; set; }
+        public Verb Verb { get; set; }
+        public Verb? Verb2 { get; set; }
+        public bool IsDoubleVerb { get; set; } = false;
+        public Tragedy Tragedy { get; set; }
+
+
+    }
+
+    public enum Subject
+    {
+        [Description("I")]
+        I,
+        [Description("My wife and I")]
+        WifeAndI,
+        [Description("My husband and I")]
+        HusbandAndI,
+        [Description("We")]
+        We
+    }
+
+    public enum Verb 
+    { 
+        [Description("hearbroken")]
+        Heartbroken,
+        [Description("devastated")]
+        Devastated,
+        [Description("horrified")]
+        Horrified,
+    }
+
+    public enum Tragedy
+    {
+        SenseLessTragedy,
+        LossOfLife,
+        Shooting
+    }
+}
