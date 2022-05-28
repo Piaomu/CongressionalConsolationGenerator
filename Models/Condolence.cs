a8a794e4eb593d12d4312d8d43935c3e009b74f7
piaomu <driftwood.wahl@gmail.com>
2022-05-28 11:03:44 -0500

    Update models

--- Models/Condolence.cs
+++ Models/Condolence.cs
@@ -5,21 +5,22 @@ namespace CongressionalConsolationGenerator.Models
 {
     public class Condolence
     {
+        public string Id { get; set; }
         public Subject SentenceSubject { get; set; }
         public Verb Verb { get; set; }
         public Verb? Verb2 { get; set; }
         public bool IsDoubleVerb { get; set; } = false;
         public Tragedy Tragedy { get; set; }
+        public Year Year { get; set; }
         public string? Location { get; set; }
         public string? State { get; set; }
         public DateTime Date { get; set; }
-        public Year Year { get; set; }
         public Subject Sentence2Subject { get; set; }
         public ThoughtPrayer ThoughtsAndPrayers { get; set; }
         public SentenceObject Sentence2Object { get; set; }
         public Adjective ResponderAdjective { get; set; }
         public Heroes Heroes { get; set; }
-
+        public string Signature { get; set; }
     }
 
     public enum Subject
